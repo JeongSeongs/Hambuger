452cea242cc07aad9c06c960d604645bf5fbbbb6
JeongSeongs <130660241+JeongSeongs@users.noreply.github.com>
2023-05-31 00:20:01 +0900

    CSS:Contact

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+
+
+const homeBtn = document.querySelector(".home__btn");
+homeBtn.addEventListener("click",()=>{
+    const scrollTo = document.querySelector("#contact");
+    scrollTo.scrollIntoView({behavior:"smooth"});
+})
+
+
+const nav = document.querySelector("#navbar");
+const navHeight = nav.getBoundingClientRect().height;
+document.addEventListener("scroll",()=>{
+    if(window.scrollY> navHeight){
+        nav.classList.add("dark");
+    }else{
+        nav.classList.remove("dark");
+    }
+})
