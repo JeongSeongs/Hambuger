a3032501efcdd2f5338fcf8d4931618141ef3458
JeongSeongs <130660241+JeongSeongs@users.noreply.github.com>
2023-05-31 00:51:16 +0900

    JS:finish

--- main.js
+++ main.js
@@ -16,3 +16,33 @@ document.addEventListener("scroll",()=>{
         nav.classList.remove("dark");
     }
 })
+
+const home = document.querySelector("#home");
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener("scroll",()=>{
+    home.style.opacity = 1 - window.scrollY/homeHeight;
+})
+
+const arrow = document.querySelector(".arrow__up");
+
+document.addEventListener("scroll",()=>{
+    if(window.scrollY>homeHeight/2){
+        arrow.classList.add("visiable")
+    }else{
+        arrow.classList.remove("visiable")
+    }
+})
+
+arrow.addEventListener("click",()=>{
+    const scrollTo = document.querySelector("#home");
+    scrollTo.scrollIntoView({behavior:"smooth"});
+})
+
+const navabarMenu = document.querySelector(".navbar__menu");
+navabarMenu.addEventListener("click",(e)=>{
+    const scrollTo =document.querySelector(e.target.dataset.link);
+    scrollTo.scrollIntoView({behavior:"smooth"});
+    
+})
+
+    
